9e7327ead6319f40f63a7eb9fbdb610f25b2ecf9
Mali3454 <wojtek.buchwal@gmail.com>
2023-03-31 00:01:48 +0200

    update

---
+++ build/assets/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.portfolio = exports.transactionHistory = exports.sass = exports.tailwind = exports.bootstrap = exports.ts = exports.js = exports.git = exports.react = exports.html = exports.node = exports.css = undefined;
+
+var _css = require('./css.png');
+
+var _css2 = _interopRequireDefault(_css);
+
+var _node = require('./node.png');
+
+var _node2 = _interopRequireDefault(_node);
+
+var _html = require('./html.png');
+
+var _html2 = _interopRequireDefault(_html);
+
+var _react = require('./react.png');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _git = require('./git.png');
+
+var _git2 = _interopRequireDefault(_git);
+
+var _javascript = require('./javascript.png');
+
+var _javascript2 = _interopRequireDefault(_javascript);
+
+var _typescript = require('./typescript.png');
+
+var _typescript2 = _interopRequireDefault(_typescript);
+
+var _sass = require('./sass.png');
+
+var _sass2 = _interopRequireDefault(_sass);
+
+var _tailwind = require('./tailwind.png');
+
+var _tailwind2 = _interopRequireDefault(_tailwind);
+
+var _bootstrap = require('./bootstrap.png');
+
+var _bootstrap2 = _interopRequireDefault(_bootstrap);
+
+var _transaction = require('./transaction.png');
+
+var _transaction2 = _interopRequireDefault(_transaction);
+
+var _portfolio = require('./portfolio.png');
+
+var _portfolio2 = _interopRequireDefault(_portfolio);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.css = _css2.default;
+exports.node = _node2.default;
+exports.html = _html2.default;
+exports.react = _react2.default;
+exports.git = _git2.default;
+exports.js = _javascript2.default;
+exports.ts = _typescript2.default;
+exports.bootstrap = _bootstrap2.default;
+exports.tailwind = _tailwind2.default;
+exports.sass = _sass2.default;
+exports.transactionHistory = _transaction2.default;
+exports.portfolio = _portfolio2.default;
